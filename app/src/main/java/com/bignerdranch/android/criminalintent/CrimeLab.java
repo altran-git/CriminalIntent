57a2a48b5800a2b0b29220dc290339993cdddab0
Andy Tran <trnandy@gmail.com>
2016-08-21 20:56:36 -0700

    feat: Implemented Initial DB
    page 440-443

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -1,6 +1,9 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.bignerdranch.android.criminalintent.database.CrimeBaseHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +16,10 @@ import java.util.UUID;
 //Crimelab as a Singleton implementation
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
+
     private List<Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public static CrimeLab get(Context context) {
         if(sCrimeLab == null){
@@ -24,6 +30,9 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context){
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext)
+                .getWritableDatabase();
         mCrimes = new ArrayList<>();
     }
 
---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeBaseHelper.java
@@ -0,0 +1,32 @@
+package com.bignerdranch.android.criminalintent.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by ND on 8/21/2016.
+ */
+public class CrimeBaseHelper extends SQLiteOpenHelper {
+    private static final int VERSION = 1;
+    private static final String DATABASE_NAME = "crimeBase.db";
+
+    public CrimeBaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + CrimeDbSchema.CrimeTable.NAME + "(" +
+                " _ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                CrimeDbSchema.CrimeTable.Cols.UUID + ", " +
+                CrimeDbSchema.CrimeTable.Cols.TITLE + ", " +
+                CrimeDbSchema.CrimeTable.Cols.DATE + ", " +
+                CrimeDbSchema.CrimeTable.Cols.SOLVED + ")");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
